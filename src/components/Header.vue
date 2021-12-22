604e8f172b68093aa9ff647004bc6577a65e7dc8
caiosfg <caio.sfg@gmail.com>
2021-12-22 15:19:24 -0300

    alguns componentes inseridos

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Trading App</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home</h1>
+  <div>
+    <p>{{ text }}</p>
+    <img :src="img.response" :alt="img.text">
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: "Olá seja bem vindo",
+      img:{ response: './assets/graphic.jpg', text: 'graphic trade'}
+    };
+  },
+};
+</script>
