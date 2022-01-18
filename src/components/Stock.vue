ded2794de249f7cb5da6402f103c5b9e315244a1
caiosfg <caio.sfg@gmail.com>
2022-01-18 00:24:25 -0300

    dados dinamicos

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -2,13 +2,13 @@
     <v-flex class="pr-3 pb-3" xs12 md6 lg4>
         <v-card class="green darken-3 white--text">
             <v-card-title class="headline">
-                <strong>NOME <small>(Preço: PRECO)</small></strong>
+                <strong>{{ stock.name }} <small>(Preço: {{ stock.price }})</small></strong>
             </v-card-title>
         </v-card>
         <v-card>
             <v-container fill-height>
-                <v-text-field label="Quantidade" type="number" />
-                <v-btn class="green darken-3 white--text">
+                <v-text-field label="Quantidade" type="number" v-model.number="quantity" />
+            <v-btn class="green darken-3 white--text" :disabled="quantity <= 0 || !Number.isInteger(quantity)" @click="buyStock">
                     Comprar
                 </v-btn>
             </v-container>
@@ -17,7 +17,22 @@
 </template>
 <script>
 export default {
-    data: () => ({
-    })
+    props: ['stock'],
+    data(){
+        return {
+            quantity: 0
+        }
+    },
+    methods:{
+        buyStock(){
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            console.log (order)
+            this.quantity = 0
+        }
+    }
 }
 </script>
