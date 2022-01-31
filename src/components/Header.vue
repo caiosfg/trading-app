ce06072cb5c1cfcfcc487f9f677987c45f11ebcd
caiosfg <caio.sfg@gmail.com>
2022-01-31 17:59:04 -0300

    algumas insercoes realizadas

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,7 +19,7 @@
                     <v-btn v-bind="attrs" v-on="on">Salvar & Carregar</v-btn>
                 </template>
                 <v-list>
-                    <v-list-item>
+                    <v-list-item @click="saveData">
                         <v-list-item-title>Salvar Dados</v-list-item-title>
                     </v-list-item>
                     <v-list-item>
@@ -48,6 +48,11 @@ export default {
         ...mapActions(['randomizeStocks']),
         endDay(){
             this.randomizeStocks()
+        },
+        saveData(){
+            const { funds, stockPortfolio, stocks } = this.$store.getters
+
+            this.$http.put('data.json', { funds, stockPortfolio, stocks })
         }
     }
 }
